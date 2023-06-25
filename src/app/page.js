d3d12fee08850ccb5fceb57edd22f103af7b70f2
gitrenzo <renzoelm25@gmail.com>
2023-06-25 12:55:11 -0400

    First commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { outfit } from './utils/fonts'
+
+
+export default function Home() {
+  return (
+    <main className={outfit.className}>
+
+      <h1>Hola mundo en nextjs</h1>
+
+    </main>
+  )
+}
